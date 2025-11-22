780692feaf4a72b2c255564b50f12dbda3810347
Chris Eberle <chris@withampersand.com>
2025-11-22 14:01:39 -0800

    fix bug

--- server/tools.go
+++ server/tools.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+
 	"github.com/viant/jsonrpc"
 	"github.com/viant/mcp-protocol/schema"
 )
@@ -11,9 +12,13 @@ import (
 // ListTools handles the tools/list method
 func (h *Handler) ListTools(ctx context.Context, request *jsonrpc.Request) (*schema.ListToolsResult, *jsonrpc.Error) {
 	listToolsRequest := &schema.ListToolsRequest{Method: request.Method}
-	if err := json.Unmarshal(request.Params, &listToolsRequest.Params); err != nil {
-		return nil, jsonrpc.NewInvalidParamsError(fmt.Sprintf("failed to parse: %v", err), request.Params)
+
+	if len(request.Params) > 0 {
+		if err := json.Unmarshal(request.Params, &listToolsRequest.Params); err != nil {
+			return nil, jsonrpc.NewInvalidParamsError(fmt.Sprintf("failed to parse: %v", err), request.Params)
+		}
 	}
+
 	id, _ := jsonrpc.AsRequestIntId(request.Id)
 	return h.handler.ListTools(ctx, &jsonrpc.TypedRequest[*schema.ListToolsRequest]{Request: listToolsRequest, Id: uint64(id)})
 }
@@ -21,9 +26,13 @@ func (h *Handler) ListTools(ctx context.Context, request *jsonrpc.Request) (*sch
 // CallTool handles the tools/call method
 func (h *Handler) CallTool(ctx context.Context, request *jsonrpc.Request) (*schema.CallToolResult, *jsonrpc.Error) {
 	callToolRequest := &schema.CallToolRequest{Method: request.Method}
-	if err := json.Unmarshal(request.Params, &callToolRequest.Params); err != nil {
-		return nil, jsonrpc.NewInvalidParamsError(fmt.Sprintf("failed to parse: %v", err), request.Params)
+
+	if len(request.Params) > 0 {
+		if err := json.Unmarshal(request.Params, &callToolRequest.Params); err != nil {
+			return nil, jsonrpc.NewInvalidParamsError(fmt.Sprintf("failed to parse: %v", err), request.Params)
+		}
 	}
+
 	id, _ := jsonrpc.AsRequestIntId(request.Id)
 	return h.handler.CallTool(ctx, &jsonrpc.TypedRequest[*schema.CallToolRequest]{Request: callToolRequest, Id: uint64(id)})
 }
